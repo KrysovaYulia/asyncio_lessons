26f1ddca72513fe728446cddf11346e6ff143985
yuliakrysova <kryuma2oos@gmail.com>
2025-05-30 21:54:08 +0300

    создание задачи

---
+++ 2_7.py
@@ -0,0 +1,19 @@
+import asyncio
+from util import delay
+
+
+async def add_one(number):
+	return number + 1
+
+async def hello_world_message():
+	await delay(1)
+	return 'Hello world!'
+
+
+async def main():
+	message = await hello_world_message()
+	one_plus_one = await add_one(1)
+	print(one_plus_one)
+	print(message)
+
+asyncio.run(main())
